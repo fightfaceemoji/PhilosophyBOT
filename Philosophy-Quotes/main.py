26a1e3f060d98958d1332ae51589406054fcc396
Crull, Amy <89597544+fightfaceemoji@users.noreply.github.com>
2021-10-13 10:06:42 -0500

    Add files via upload

---
+++ Philosophy-Quotes/main.py
@@ -0,0 +1,130 @@
+import discord 
+import os
+import random
+from keep_alive import keep_alive
+
+
+client = discord.Client()
+
+L = [
+ '“The unexamined life is not worth living” – Socrates.',
+  '“Whereof one cannot speak, thereof one must be silent” – Ludwig Wittgenstein.',
+  '“Entities should not be multiplied unnecessarily” – William of Ockham.',
+  '“The life of man (in a state of nature) is solitary, poor, nasty, brutish, and short” – Thomas Hobbes.',
+ '“I think, therefore I am” (“Cogito, ergo sum”) – René Descartes',
+ '“He who thinks great thoughts, often makes great errors” – Martin Heidegger',
+ '“We live in the best of all possible worlds” – Gottfried Wilhelm Leibniz',
+ '“What is rational is actual and what is actual is rational” – G. W. F. Hegel',
+  '“God is dead! He remains dead! And we have killed him.” – Friedrich Nietzsche',
+  '“There is but one truly serious philosophical problem, and that is suicide” – Albert Camus',
+  '“One cannot step twice in the same river” – Heraclitus',
+  '“The greatest happiness of the greatest number is the foundation of morals and legislation” – Jeremy Bentham',
+  '“To be is to be perceived” (“Esse est percipi”)– Bishop George Berkeley',
+  '“Happiness is not an ideal of reason but of imagination” – Immanuel Kant',
+  '“No mans knowledge here can go beyond his experience” – John Locke',
+  '“God is not willing to do everything, and thus take away our free will and that share of glory which belongs to us” – Niccolo Machiavelli',
+  '“Liberty consists in doing what one desires” – John Stuart Mill',
+  '“It is undesirable to believe a proposition when there is no ground whatever for supposing it true” – Bertrand Russell',
+  '“Even while they teach, men learn” – Seneca the Younger',
+  '“There is only one good, knowledge, and one evil, ignorance” – Socrates',
+  '“If God did not exist, it would be necessary to invent Him” – Voltaire',
+  '“This is patently absurd; but whoever wishes to become a philosopher must learn not to be frightened by absurdities” – Bertrand Russell',
+  '“One cannot conceive anything so strange and so implausible that it has not already been said by one philosopher or another” – René Descartes',
+  '“Leisure is the mother of philosophy” – Thomas Hobbes',
+  '“Philosophy is a battle against the bewitchment of our intelligence by means of language” – Ludwig Wittgenstein',
+  '“There is only one thing a philosopher can be relied upon to do, and that is to contradict other philosophers” – William James',
+  '“We are what we repeatedly do. Excellence, then, is not an act, but a habit” – Aristotle',
+  '“Only one man ever understood me, and he didn’t understand me” – G. W. F. Hegel',
+  '“The mind is furnished with ideas by experience alone” – John Locke',
+  '“Life must be understood backward. But it must be lived forward ” – Søren Kierkegaard',
+  '“Science is what you know. Philosophy is what you dont know” – Bertrand Russell',
+  '“Metaphysics is a dark ocean without shores or lighthouse, strewn with many a philosophic wreck” – Immanuel Kant',
+  '“Philosophy is at once the most sublime and the most trivial of human pursuits” – William James',
+  '“History is Philosophy teaching by examples” – Thucydides',
+  '“He who is unable to live in society, or who has no need because he is sufficient for himself, must be either a beast or a god” – Aristotle',
+  '“You can discover more about a person in an hour of play than in a year of conversation” – Plato',
+  '“Things alter for the worse spontaneously, if they be not altered for the better designedly” – Francis Bacon',
+  '“All that is necessary for the triumph of evil is that good men do nothing” – mistakenly attributed to Edmund Burke',
+  '“Is man merely a mistake of Gods? Or God merely a mistake of mans?” – Friedrich Nietzsche',
+  '“I would never die for my beliefs because I might be wrong” – Bertrand Russell',
+  '“Religion is the sign of the oppressed ... it is the opium of the people” – Karl Marx',
+  '“Happiness is the highest good” – Aristotle',
+  '“If men were born free, they would, so long as they remained free, form no conception of good and evil” – Baruch Spinoza',
+  '“The greater the difficulty, the more glory in surmounting it” – Epicurus',
+  '“Whatever is reasonable is true, and whatever is true is reasonable” – G. W. F. Hegel',
+  '“Morality is not the doctrine of how we may make ourselves happy, but of how we may make ourselves worthy of happiness” – Immanuel Kant',
+  '“Man is condemned to be free” – Jean-Paul Sartre',
+  '“It is one thing to show a man that he is in error, and another to put him in possession of truth” – John Locke',
+  '“I don’t know why we are here, but I’m pretty sure it is not in order to enjoy ourselves” – Ludwig Wittgenstein',
+  '“That man is wisest who, like Socrates, realizes that his wisdom is worthless” – Plato',
+  '“The only thing I know is that I know nothing” – Socrates',
+  '“All is for the best in the best of all possible worlds” – Voltaire (in parody of Leibniz)',
+  '“The function of prayer is not to influence God, but rather to change the nature of the one who prays” – Søren Kierkegaard',
+  '“Man is born free, but is everywhere in chains” – Jean-Jacques Rousseau',
+  '“Man will never be free until the last king is strangled with the entrails of the last priest” – Denis Diderot',
+  '“If you would be a real seeker after truth, it is necessary that at least once in your life you doubt, as far as possible, all things” – René Descartes',
+  '“Happiness lies in virtuous activity, and perfect happiness lies in the best activity, which is contemplative” – Aristotle',
+  '“I can control my passions and emotions if I can understand their nature” – Spinoza',
+  '“Philosophers have hitherto only interpreted the world in various ways; the point, however, is to change it” – Karl Marx',
+  '“It is wrong always, everywhere and for everyone, to believe anything upon insufficient evidence” – W. K. Clifford',
+  '“Virtue is nothing else than right reason” – Seneca the Younger',
+  '“Freedom is secured not by the fulfilling of ones desires, but by the removal of desire” – Epictetus',
+  '“In everything, there is a share of everything” – Anaxagoras',
+  '“A little philosophy inclineth mans mind to atheism; but depth in philosophy bringeth men’s minds about to religion” – Sir Francis Bacon',
+  '“The brave man is he who overcomes not only his enemies but his pleasures” – Democritus',
+  '“Good and evil, reward and punishment, are the only motives to a rational creature” – John Locke',
+  '“To do as one would be done by, and to love ones neighbor as oneself, constitute the ideal perfection of utilitarian morality” – John Stuart Mill',
+  '“Everything that exists is born for no reason, carries on living through weakness, and dies by accident” – Jean-Paul Sartre',
+  '“Man is the measure of all things” – Protagoras',
+  '“We are too weak to discover the truth by reason alone” – St. Augustine',
+  '“The mind is furnished with ideas by experience alone” – John Locke',
+  ' "Human life is suffering" (Vīta hūmāna est supplicim) - Seneca',
+  ' "All men by nature desire to know. An indication of this is the delight we take in our senses; for even apart from their usefulness they are loved for themselves..." - Aristotle',
+  '"Men are so simple of mind, and so much dominated by their immediate needs, that a decietful man will always find plenty who are ready to be deceived." - Niccolò Machiavelli',
+  '"Faith and reason are like two wings on which the human spirit rises to the contemplation of truth; and God has placed in the human heart a desire to know the truth - in a word, to know Himself - so that, by knowing and loving God, men and women may also come to the fullness of truth about themselves." - Pope John Paul II',
+  ' "Only see how large the soul is! - that if a man is drowned in the sea, what a great bubble comes up out of the top of the water! ... this is wind - is the soul that is the last to ascend out of the deep to glory ... the bubble is the soul" - Lord Timothy Dexter',
+  '“My friends & fellow mortals there is A first Cose of all things” - Lord Timothy Dexter'
+]
+
+R = [
+  'What do you think of this?',
+  'Do you agree? Why or Why not?',
+  'What would your favorite philosopher think of this quote?',
+  'Why do you think this philosopher thought this?',
+  'Is this thought compatible with your view of the human person?',
+  'Unto what end did this philosopher aim?',
+  'How does this relate to your personal final cause or end?',
+  'What are the implications of this quote?',
+  'What does this philosopher taken for granted in their argument?',
+  'Is this quote helpful to your understanding of the intended topic?'
+]
+
+@client.event
+async def on_ready():
+  print('Time for {0.user}'.format(client))
+  await client.change_presence(activity=discord.Game(name='Existentialism 2.0'))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('hi'):
+    await message.channel.send('hello there')
+
+  if message.content.startswith('*quote'):
+    n = random.randint(0,len(L))
+    await message.channel.send(L[n]) 
+
+  if message.content.startswith('*daily quote'):
+   n = random.randint(0,len(L))
+   await message.channel.send(L[n])
+   r = random.randint (0,len(R))
+   await message.channel.send(R[r])
+   await message.channel.send('<@&839612636983197716>')
+     
+
+keep_alive()
+
+client.run(os.getenv('TOKEN'))
