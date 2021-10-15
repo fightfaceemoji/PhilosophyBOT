50809cedce5533b827008688d1e085accaf548d4
fightface <crulla@stthom.edu>
2021-10-15 13:23:45 -0500

    apparently learning .js now

--- Philosophy-Quotes/socrates.js
+++ Philosophy-Quotes/socrates.js
@@ -1,41 +1,14 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const Discord = require("discord.js"); // Requires the npm package 'discord.js'.
+const client = new Discord.Client(); // Create an instance of Discord#Client
+const config = require("./config.json");
+client.login(config.token); // Uses value of key 'token' in config file.
 
-const commands = [{
-  name: 'ping',
-  description: 'Replies with Pong!'
-}];
-
-const rest = new REST({ version: '9' }).setToken('token');
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands },
-    );
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
-})();
-
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("message", (message) => {
+if(message.content == "!ping"){ // Check if content of message is "!ping"
+		message.channel.send("pong!"); // Call .send() on the channel object the message was sent in
+	}
+if("guildMemberAdd", (member) => { // EventEmitter, nothing new
+        message.channel.send("HI!")
+    }
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
-
-client.login('token');
